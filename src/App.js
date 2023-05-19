7fcaec7ebc171b32232d137ac4a305a3d10fb78d
Abhinav <abhinavkinarullathil@gmail.com>
2023-05-19 13:16:18 +0530

    First Commit & Components added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter as Router,Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Route>
+        
+      </Route>
+    </Router>
+  );
+}
+
+export default App;
